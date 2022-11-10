b7ab77e73dffefb4701100f1646a247a13715248
luciarigoni <luciarigoni@live.com>
2022-11-10 15:04:45 -0300

    Initial Commit

---
+++ MyFavoriteDishesSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFavoriteDishesSwiftUI
+//
+//  Created by Lucia Rigoni Freire on 10/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyFavoriteDishesSwiftUI/MyFavoriteDishesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFavoriteDishesSwiftUIApp.swift
+//  MyFavoriteDishesSwiftUI
+//
+//  Created by Lucia Rigoni Freire on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MyFavoriteDishesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
